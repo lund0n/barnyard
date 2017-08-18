f5726ab2637e4f2f3f3fa90b5b31fecb4bfe1375
Jeremy Lund <jeremylund@workfront.com>
2017-08-17 21:32:03 -0500

    refactor: move state reducers to use flowRight

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import { Observable, Scheduler } from 'rxjs/Rx'
+import _ from 'lodash'
 import sceneRenderer from './scene-renderer'
 import { steer } from './keycode-handlers'
 import { isOutOfBounds } from './util'
 
+const WIDTH = 500
+const HEIGHT = 400
+const FRAME_RATE = 60 // frames/second
+const VELOCITY = 100 // pixels/second
+
 const canvas = document.createElement('canvas')
-canvas.width = 500
-canvas.height = 400
+canvas.width = WIDTH
+canvas.height = HEIGHT
 document.body.appendChild(canvas)
 
 const renderScene = sceneRenderer(canvas)
 
-const FRAME_RATE = 30 // frames/second
-const VELOCITY = 100 // pixels/second
 const gameRate = (frameRate = 60, scheduler = Scheduler.animationFrame) =>
 	Observable.interval(1000 / frameRate, scheduler).map(() => scheduler.now())
 
@@ -28,6 +32,7 @@ const vector$ = Observable.combineLatest(
 	(velocity, { x, y }) => ({
 		x: velocity * x,
 		y: velocity * y,
+		bearing: x !== 0 ? 'H' : y !== 0 ? 'V' : '?',
 	})
 ).sample(gameRate$)
 
@@ -37,27 +42,28 @@ const start$ = Observable.of(function() {
 		alive: true,
 		x: 400,
 		y: 300,
+		bearing: '?',
 	}
 })
 const position$ = vector$.map(
-	({ x, y }) =>
+	({ x, y, bearing }) =>
 		function(state) {
-			return { ...state, x: state.x + x, y: state.y + y }
+			return { ...state, x: state.x + x, y: state.y + y, bearing }
 		}
 )
+const isNotInBounds = (width, height) => state => {
+	if (state.alive) {
+		return {
+			...state,
+			alive: !isOutOfBounds(state.x, state.y, width, height),
+		}
+	}
+	return state
+}
+const stateReducers = _.flowRight(isNotInBounds(WIDTH, HEIGHT))
+
 Observable.merge(start$, position$)
 	.scan((state, reducer) => {
-		// Changes based on outside actions.
-		const nextState = reducer(state)
-		// Changes based on state changes.
-		if (nextState.alive) {
-			nextState.alive = !isOutOfBounds(
-				nextState.x,
-				nextState.y,
-				canvas.width,
-				canvas.height
-			)
-		}
-		return nextState
+		return stateReducers(reducer(state))
 	}, {})
 	.subscribe(renderScene)
